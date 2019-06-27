f2aaa94b936613424952e1e77112f24801d82b6b
a378042176 <378042176@qq.com>
2019-06-27 17:59:38 +0800

    添加猜你喜欢

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import Category from "./components/Category";
 import HeadLine from "./components/Headline";
+import Discount from "./components/Discount";
+import LikeList from "./components/LikeList";
 class Home extends Component {
   render() {
     return (
       <div>
         <Category />
         <HeadLine />
+        <Discount />
+        <LikeList />
       </div>
     );
   }
