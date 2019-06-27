bd6662c3793f84fda8070ccd679f6f5ca528fee3
a378042176 <378042176@qq.com>
2019-06-27 16:43:58 +0800

    添加点评头条

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Category from "./components/Category";
+import HeadLine from "./components/Headline";
 class Home extends Component {
   render() {
     return (
       <div>
         <Category />
+        <HeadLine />
       </div>
     );
   }
