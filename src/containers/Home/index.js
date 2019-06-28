a40f0418f100e971de43310cbed2c1feb0f87f9a
a378042176 <378042176@qq.com>
2019-06-28 17:07:16 +0800

    首页其他组件开发

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,14 +3,22 @@ import Category from "./components/Category";
 import HeadLine from "./components/Headline";
 import Discount from "./components/Discount";
 import LikeList from "./components/LikeList";
+import HomeHeader from "./components/HomeHeader";
+import Banner from "./components/Banner";
+import Activity from "./components/Activity";
+import Footer from "../../components/Footer";
 class Home extends Component {
   render() {
     return (
       <div>
+        <HomeHeader />
+        <Banner />
         <Category />
         <HeadLine />
+        <Activity />
         <Discount />
         <LikeList />
+        <Footer />
       </div>
     );
   }
