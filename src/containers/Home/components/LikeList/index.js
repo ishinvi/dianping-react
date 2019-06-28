bd621124125e09450a93591b79edb447a111bb4c
a378042176 <378042176@qq.com>
2019-06-28 16:37:48 +0800

    加载更多功能实现

--- src/containers/Home/components/LikeList/index.js
+++ src/containers/Home/components/LikeList/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import LikeItem from '../LikeItem'
-import './style.css'
+import LikeItem from "../LikeItem";
+import Loading from "../../../../components/Loading";
+import "./style.css";
+
 const dataSource = [
   {
     id: "p-1",
@@ -65,19 +67,70 @@ const dataSource = [
 ];
 
 class LikeList extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef(); //在下面引用，相当于myRef等于div的dom元素
+    this.state = {
+      data: dataSource,
+      loadTimes: 1
+    };
+    this.removeListener = false;
+  }
+
   render() {
-    const data = dataSource;
+    const { data, loadTimes } = this.state;
     return (
-      <div className="likeList">
+      <div ref={this.myRef} className="likeList">
         <div className="likeList__header">猜你喜欢</div>
         <div className="likeList__list">
           {data.map((item, index) => {
-            return <LikeItem key={item.id} data={item} />;
+            return <LikeItem key={index} data={item} />;
           })}
         </div>
+        {loadTimes < 3 ? (
+          <Loading />
+        ) : (
+          <a className="likeList__viewAll">查看更多</a>
+        )}
       </div>
     );
   }
+
+  componentDidMount() {
+    document.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.loadTimes >= 3 && !this.removeListener) {
+      document.removeEventListener("scroll", this.handleScroll);
+      this.removeListener = true;
+    }
+  }
+  componentWillUnmount() {
+    if (!this.removeListener) {
+      document.removeEventListener("scroll", this.handleScroll);
+      this.removeListener = true;
+    }
+  }
+
+  //处理屏幕滚动事件，实现加载更多的效果
+  handleScroll = () => {
+    const scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    const screenHeight = document.documentElement.clientHeight;
+    const likeListTop = this.myRef.current.offsetTop;
+    const likeListHeight = this.myRef.current.offsetHeight;
+    if (scrollTop >= likeListHeight + likeListTop - screenHeight) {
+      const newData = this.state.data.concat(dataSource);
+      const newLoadTimes = this.state.loadTimes + 1;
+      setTimeout(() => {
+        this.setState({
+          data: newData,
+          loadTimes: newLoadTimes
+        });
+      }, 1000);
+    }
+  };
 }
 
 export default LikeList;
