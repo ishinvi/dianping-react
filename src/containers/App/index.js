02ca2a059ac4592afffcfc680d21726dc5b44c8e
a378042176 <378042176@qq.com>
2019-07-01 18:25:41 +0800

    团购基本信息

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import ErrorToast from "../../components/ErrorToast";
 import { actions as appActions, getError } from "../../redux/modules/app";
 import Home from "../Home";
+import ProductDetail from "../ProductDetail";
 class App extends Component {
   render() {
     const {
@@ -15,6 +16,7 @@ class App extends Component {
       <div className="App">
         <Router>
           <Switch>
+            <Route path="/detail/:id" component={ProductDetail} />
             <Route path="/" component={Home} />
           </Switch>
         </Router>
