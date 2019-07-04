a2543cd63b349cc95bdc455b11fe55e282e4ab2c
shinvi <378042176@qq.com>
2019-07-04 23:33:59 +0800

    用户中心组件开发

--- src/containers/User/index.js
+++ src/containers/User/index.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import UserMain from "./components/UserMain";
+import UserHeader from "./components/UserHeader";
 class User extends Component {
-    render() {
-        return (
-            <div>
-                用户中心
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <UserHeader onBack={this.handleBack} onLogout={this.handleLogout} />
+        <UserMain />
+      </div>
+    );
+  }
+
+  handleBack=()=>{
+    //todo
+  }
+  handleLogout=()=>{
+    //todo
+  }
 }
 
 export default User;
