e35451d0aa6d731c38cf0e8afd030e5996249c98
a378042176 <378042176@qq.com>
2019-07-04 16:41:47 +0800

    登录 redux模块设计

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -4,7 +4,15 @@ import home from "./home";
 import detail from "./detail";
 import app from "./app";
 import search from "./search";
+import login from "./login";
 //合并成根reducer
 
-const rootReducer = combineReducers({ entities, home, detail, app, search }); //mmp 不加花括号会报Cannot read property 'error' of undefined
+const rootReducer = combineReducers({
+  entities,
+  home,
+  detail,
+  app,
+  search,
+  login
+}); //mmp 不加花括号会报Cannot read property 'error' of undefined
 export default rootReducer;
