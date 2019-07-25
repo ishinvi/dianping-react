1f674e50312cac09535e91541f388071960f1db3
a378042176 <378042176@qq.com>
2019-07-25 16:01:11 +0800

    下单数据和个人中心页同步

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -12,9 +12,16 @@ import {
 import { actions as commentActions } from "./entities/comments";
 import { combineReducers } from "redux";
 
+const typeToKey = {
+  [TO_PAY_TYPE]: "toPayIds",
+  [AVAILABLE_TYPE]: "availableIds",
+  [REFUND_TYPE]: "refundIds"
+};
+
 const initialState = {
   orders: {
     isFetching: false,
+    fetched: false, //购买页面使用
     ids: [],
     toPayIds: [], //待付款的订单id
     availableIds: [], //可使用的订单id
@@ -63,8 +70,8 @@ export const actions = {
   //获取订单列表
   loadOrders: () => {
     return (dispatch, getState) => {
-      const { ids } = getState().user.orders;
-      if (ids.length > 0) {
+      const { ids, fetched } = getState().user.orders;
+      if (fetched) {
         return null;
       }
       const endpoint = url.getOrders();
@@ -188,6 +195,7 @@ const orders = (state = initialState.orders, action) => {
       return {
         ...state,
         isFetching: false,
+        fetched: true,
         ids: state.ids.concat(action.response.ids),
         toPayIds: state.toPayIds.concat(toPayIds),
         availableIds: state.availableIds.concat(availableIds),
@@ -202,6 +210,19 @@ const orders = (state = initialState.orders, action) => {
         availableIds: removeOrderId(state, "availableIds", action.orderId),
         refundIds: removeOrderId(state, "refundIds", action.orderId)
       };
+    case orderTypes.ADD_ORDER:
+      const { order } = action;
+      const key = typeToKey[order.type];
+      return key
+        ? {
+            ...state,
+            ids: [order.id].concat(state.ids),
+            [key]: [order.id].concat(state[key])
+          }
+        : {
+            ...state,
+            ids: [order.id].concat(state.ids)
+          };
     default:
       return state;
   }
